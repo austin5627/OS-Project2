d19e4f5d6e5c9b90c26e9759ff3de49417263b60
ethanwcooper <ethanwcooper@outlook.com>
2022-10-15 15:27:12 -0500

    hi

--- launcher.sh
+++ launcher.sh
@@ -4,12 +4,12 @@
 export netid=$(whoami)
 
 # Root directory of your project
-if [[ $netid -eq "ash170000" ]]; then
-  export PROJDIR=$HOME/CS-6378/cs6378proj2
-else
+#if [[ $netid -eq "ash170000" ]]; then
+#  export PROJDIR=$HOME/CS-6378/cs6378proj2
+#else
   export PROJDIR=$HOME/cs6378/proj2/cs6378proj2
-fi
-
+#fi
+echo $ne
 echo $PROJDIR
 # Directory where the config file is located on your local system
 export CONFIGLOCAL=./config.txt
