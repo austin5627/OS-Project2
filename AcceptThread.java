41a338856f62bfd1d4f845238e900618d98f21ab
ethanwcooper <ethanwcooper@outlook.com>
2022-10-15 16:26:37 -0500

    test

--- AcceptThread.java
+++ AcceptThread.java
@@ -28,10 +28,6 @@ public class AcceptThread extends Thread {
                 mutex.addConnection(connected_id, sc);
                 acceptNew = numConnections < (mutex.numProc - mutex.nodeID - 1);
                 System.out.println("Connection from " + connected_id);
-                System.out.println("\tnumConnections = " + numConnections);
-                System.out.println("\tnumProc = " + mutex.numProc);
-                System.out.println("\tExpected numConnections = " + (mutex.numProc - mutex.nodeID - 1));
-                System.out.println("\tacceptNew = " + (numConnections < (mutex.numProc - mutex.nodeID - 1)) + " ???? " + acceptNew);
             }
             System.out.println("Finished accepting new connections");
         } catch (Exception e) {
